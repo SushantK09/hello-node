46ba0f4e8c0832fab29ddfa3b8eb9129d314b31b
SusanK_09 <susan.khadka110332@marwadiuniversity.ac.in>
2022-09-28 12:17:56 +0530

    Update in index file

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-const hello =() =>{
-    console.log("Hello World");
-}
+const user = {
+    name: "Susan Khadka",
+    age: 19,
+    gender: "Male",
+    education: "B. Tech CE",
+    university: "Marwadi University",
+    currentYear: "3rd year",
+    country: "Nepal",
+};
 
-hello();
+const userDetails = () => {
+    
+    const userDetailString = `My name is ${user.name} and I am from ${user.country}.`+
+    ` I am ${user.age} years old. I am currently studying ${user.education} - ${user.currentYear} at ${user.university}.`;
+    return userDetailString;
+};
+
+console.log(userDetails());
